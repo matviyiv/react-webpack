1fa886b1c1d63cc5ed7db40efbd93ab6106ff8ce
Nazar Matviyiv <nazar.matviyiv@auctionata.com>
2016-01-27 22:01:47 +0100

    Clean up

--- make-webpack-config.js
+++ make-webpack-config.js
@@ -141,18 +141,13 @@ function baseLoaders(options) {
     babe6HotLoader = {
       presets: ['es2015', 'stage-0', 'react'],
       env: {
-        // only enable it when process.env.NODE_ENV is 'development' or undefined
         development: {
           plugins: [['react-transform', {
             transforms: [{
               transform: 'react-transform-hmr',
-              // if you use React Native, pass 'react-native' instead:
               imports: ['react'],
-              // this is important for Webpack HMR:
               locals: ['module']
             }]
-            // note: you can put more transforms into array
-            // this is just one of them!
           }]]
         }
       }
